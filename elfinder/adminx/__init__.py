db639a4525d76a52009b1b34809b1e8e7bccd283
alexsilva <geniofuturo@gmail.com>
2019-08-05 15:59:08 -0300

    Setup display and search fields.

--- elfinder/adminx/__init__.py
+++ elfinder/adminx/__init__.py
@@ -2,6 +2,17 @@ from xadmin.sites import site
 from elfinder.adminx.mptt_adminx import MPTTModelAdmin
 from elfinder.models import File, Directory, FileCollection
 
-site.register(Directory, MPTTModelAdmin)
+
+class mMPTTModelAdmin(MPTTModelAdmin):
+    list_display = (
+        "name",
+        "collection"
+    )
+    search_fields = (
+        'name',
+    )
+
+
+site.register(Directory, mMPTTModelAdmin)
 site.register(FileCollection)
 site.register(File)

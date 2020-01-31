7b6ec9f5bd7a6e386e5cfa25da4bcc0de119a9e2
alex <geniofuturo@gmail.com>
2020-01-31 14:58:24 -0300

    v2.2.2

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ class egg_info_with_submodules(egg_info):
 setup(
     cmdclass={"build": build_with_submodules, "egg_info": egg_info_with_submodules},
     name='django-elfinder',
-    version='2.2.1',
+    version='2.2.2',
     description='Django connector for elFinder 2 - with support for FS storage and TinyMCE connector',
     long_description=README,
     author='Martin Bohacek',
